a39f5ff378b51eaba256c31bdea6789990f1e0c0
Suhejl <suhejl.asani@edu.tbz.ch>
2019-05-10 13:55:03 +0200

    dxtcfzvgbhunjmk

---
+++ Source/projects-iJ/Zeiterfassung_JavaFX/src_java/src/simpleTests/Main1.java
@@ -0,0 +1,20 @@
+package simpleTests;
+
+import java.sql.*;
+import java.util.LinkedList;
+
+public class Main1 {
+
+  public LinkedList getEntries(){
+    Connection connection = null;
+    LinkedList entries = new LinkedList();
+    try {
+      connection = DriverManager.getConnection("jdbc:mariadb://192.168.30.145:3306/", "root", "!30DBrootPW48");
+
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+    return entries;
+  }
+
+}
