a854ad3c1f94da62f654eb00c7b38b51228b3a28
Suhejl <suhejl.asani@edu.tbz.ch>
2019-05-10 08:17:37 +0200

    Here User POJO

---
+++ Source/projects-iJ/Zeiterfassung_JavaFX/src/sample/User.java
@@ -0,0 +1,69 @@
+package sample;
+
+public class User {
+  String id;
+  String name;
+  String firstname;
+  String email;
+  String tel;
+  String notes;
+
+  public User(String id, String name, String firstname, String email, String tel, String notes) {
+    this.id = id;
+    this.name = name;
+    this.firstname = firstname;
+    this.email = email;
+    this.tel = tel;
+    this.notes = notes;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getTel() {
+    return tel;
+  }
+
+  public void setTel(String tel) {
+    this.tel = tel;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
+
+
+}
