b131173e7fbe152b52ebabfcf5df5db6e7ad0933
Suhejl <suhejl.asani@edu.tbz.ch>
2019-05-17 08:42:24 +0200

    Login Code

--- Source/projects-iJ/Zeiterfassung_JavaFX/src_java/src/login_register/Main.java
+++ Source/projects-iJ/Zeiterfassung_JavaFX/src_java/src/login_register/Main.java
@@ -1,93 +1,200 @@
 package login_register;
+
+import javax.xml.transform.Result;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.*;
+import java.util.InputMismatchException;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class Main {
 
-    //Connection zu Datenbank
-    public Main() {
+  //Connection zu Datenbank
 
-        super();
-    }
+  public static Scanner scan = new Scanner(System.in);
+  static String setUsername;
+  static String setPassword;
+  static boolean backToMeni = false;
 
-    public static Connection connect() {
-        Connection con = null;
-        try {
+  static Connection connection = null;
+
+  public static Connection connect() {
+    Connection con = null;
+    try {
 // get a connection to database
-            con = DriverManager.getConnection("jdbc:mariadb://192.168.30.145:3306/", "root", "!30DBrootPW48");
+      con = DriverManager.getConnection("jdbc:mariadb://192.168.30.145:3306/zeiterfassung", "root", "!30DBrootPW48");
 
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-        }
-        return con;
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    return con;
+  }
+
+  public static void main(String[] args) throws SQLException {
+    //Login
+    connection = connect();
+  do {
+      int choice = menu();
+      switch (choice) {
+        case 1:
+          loginRegister();
+          break;
+        case 2:
+          login();
+          break;
+        default:
+          System.out.println("Falsch");
+      }
+      System.out.print("Wollen Sie zurück ins Menü\n>");
+      backToMeni = scan.nextBoolean();
+    } while (backToMeni);
+
+  }
+
+  public static void loginRegister() {
+    boolean isCaught = false;
+    SecureRandom random = new SecureRandom();
+
+    String saltValue = "";
+    byte[] salt = new byte[16];
+
+    System.out.print("Enter your Username: ");
+    setUsername = scan.nextLine();
+    System.out.print("Enter your Password: ");
+    setPassword = scan.nextLine();
+
+
+    MessageDigest md;
+    try {
+      // Select the message digest for the hash computation -> SHA-256
+      md = MessageDigest.getInstance("SHA-256");
+      // Generate the random salt
+      random.nextBytes(salt);
+      // Passing the salt to the digest for the computation
+      md.update(salt);
+      // Generate the salted hash
+      byte[] hashedPassword = md.digest(setPassword.getBytes(StandardCharsets.UTF_8));
+
+      StringBuilder sb = new StringBuilder();
+      for (byte b : hashedPassword)
+        sb.append(String.format("%02x", b));
+
+      setPassword = sb.toString();
+
+      for (int i = 0; i < salt.length; i++) {
+        saltValue += salt[i] + " ";
+      }
+
+      String insertCode = "INSERT INTO Login (Username, Password, Salt) VALUES ('" + setUsername + "', '" + setPassword + "', '" + saltValue + "');";
+      String checkUser = "SELECT Username, Password, Salt FROM Login WHERE Username='" + setUsername + "' AND Password='" + setPassword + "' AND Salt='" + saltValue + "';";
+
+
+      Statement insertStatement = connection.createStatement();
+      insertStatement.executeQuery(insertCode);
+      insertStatement.executeQuery(checkUser);
+
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      isCaught = true;
+    }
+    if (!isCaught) {
+      System.out.println("Account erfolgreich erstellt");
     }
 
-    public static void main(String[] args) throws SQLException {
-        //Login
+  }
 
-        String username = "";
-        String password = "";
+  public static void login() {
+    String getSalt = "";
+    String getPassword = "";
+    String getUsername = "";
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your Username: ");
-        String name = scan.nextLine();
-        System.out.print("Enter your Password: ");
-        String psw = scan.nextLine();
+    System.out.print("Enter yout Username: ");
+    setUsername = scan.nextLine();
+    System.out.print("Emter your password: ");
+    setPassword = scan.nextLine();
 
+    // StringBuilder passwordSb = new StringBuilder(setPassword);
 
-        MessageDigest md;
-        try
-        {
-            // Select the message digest for the hash computation -> SHA-256
-            md = MessageDigest.getInstance("SHA-256");
+    try {
+      String getSaltandHash = "SELECT Password, Salt, Username FROM Login WHERE Username='" + setUsername + "';";
+      Statement pwStatement = connection.createStatement();
+      ResultSet getHashSalt = pwStatement.executeQuery(getSaltandHash);
 
-            // Generate the random salt
-            SecureRandom random = new SecureRandom();
-            byte[] salt = new byte[16];
-            random.nextBytes(salt);
+      while (getHashSalt.next()) {
+        getPassword = getHashSalt.getString("Password");
+        getSalt = getHashSalt.getString("Salt");
+        getUsername = getHashSalt.getString("Username");
+      }
+      String[] getSaltArray = getSalt.split(" ");
 
-            // Passing the salt to the digest for the computation
-            md.update(salt);
+      byte[] getSaltinByte = new byte[getSaltArray.length];
+      for (int i = 0; i < getSaltArray.length; i++) {
+        getSaltinByte[i] = Byte.parseByte(getSaltArray[i]);
+      }
 
-            // Generate the salted hash
-            byte[] hashedPassword = md.digest(psw.getBytes(StandardCharsets.UTF_8));
+      MessageDigest hashPassword = MessageDigest.getInstance("SHA-256");
 
-            StringBuilder sb = new StringBuilder();
-            for (byte b : hashedPassword)
-                sb.append(String.format("%02x", b));
+      hashPassword.update(getSaltinByte);
 
-            System.out.println(sb);
+      byte[] hashedPassword = hashPassword.digest(setPassword.getBytes(StandardCharsets.UTF_8));
 
-        } catch (NoSuchAlgorithmException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+      StringBuilder pwtoHash = new StringBuilder();
 
-        //Connected zu DB und macht select statement zum herausfinden
+      for (byte b : hashedPassword)
+        pwtoHash.append(String.format("%02x", b));
 
-        Statement stmt = connect().createStatement();
-        String SQL = "SELECT * FROM Login WHERE Username='" + name + "' AND Passwort='" + psw + "'";
-        ResultSet rs = stmt.executeQuery(SQL);
+      setPassword = pwtoHash.toString();
 
-        // Passwort und Username kontrolle
-        while (rs.next()) {
-            username = rs.getString("Username");
-            password = rs.getString("Passwort");
-        }
+      if (setPassword.equals(getPassword) && setUsername.equals(getUsername)) {
+        System.out.println("Ok");
+      } else {
+        System.out.println("Username oder Passwort stimmen nicht überrein");
+      }
 
-        if (name.equals(username) && password.equals(password)) {
-            System.out.println("Successful Login!");
-        } else {
-            System.out.println("Incorrect Password or incorrect Username!");
-        }
-
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    } catch (NumberFormatException e) {
+      System.out.println("Dieser User existiert nicht");
     }
+
+  }
+
+  public static int menu() {
+    boolean repeat_menu;
+    int choice = 0;
+
+    do {
+      repeat_menu = false;
+      System.out.println("Was wollen Sie machen?");
+      System.out.println("[1] Registrieren");
+      System.out.print("[2] Login\n>");
+      try {
+        choice = scan.nextInt();
+      } catch (InputMismatchException e) {
+        System.out.println("Ungültige Eingabe, Bitte nur Zahlen. ");
+        // scan.next();
+        repeat_menu = true;
+        scan.nextLine();
+      }/* finally {
+                while (scan.hasNext("\\s")) {
+                    scan.nextLine();
+                    System.out.println("Bitte nur eine Zahl eingeben.");
+                    repeat_menu = true;
+                }
+
+            }*/
+    } while (repeat_menu);
+    scan.nextLine();
+    return choice;
+  }
 
 }
 
